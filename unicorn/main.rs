294a7eefce89f46706dd047336b98a607539dd5e
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-03-17 01:35:19 +0530

    Add commandline option parsing for unicorn bin
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- unicorn/main.rs
+++ unicorn/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+
+const VERSION: &'static str = "0.1.0";
+
+fn help() {
+    println!("unicorn :: Unified Communications Over Real-time Networks");
+    println!("Version: v{}", VERSION);
+    println!("\nAvailable commands:");
+    println!(" --version - Show program version");
+    println!(" --help    - Show this help message");
+}
+
+fn print_version() {
+    println!("{}", VERSION);
+}
+
 fn main() {
-    println!("Hello, unicorn!");
+    if let Some(arg) = env::args().nth(1) {
+        match arg.as_ref() {
+            "--version" => print_version(),
+            _ => help(),
+        }
+    } else {
+        help()
+    }
 }
