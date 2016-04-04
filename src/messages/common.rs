d33b3af82537e04dfd108f83dc0f7fc699b323d4
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-04-05 01:17:11 +0530

    Abstract out ID message and create Ready message
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- src/messages/common.rs
+++ src/messages/common.rs
@@ -13,9 +13,22 @@ pub enum Components {
     Gateway,
 }
 
-/// Heartbeat message
+/// Identify message. Used by every message that needs an ID
+#[derive(RustcEncodable, RustcDecodable)]
+pub struct ID {
+    pub id: String,
+    pub component: Components,
+}
+
+/// Heartbeat message. Used to check liveliness of connected peers.
 #[derive(RustcEncodable, RustcDecodable)]
 pub struct Heartbeat {
-    pub component: Components,
+    pub id: ID,
     pub count: i32,
+}
+
+/// Ready message. Sent when a component has connected and is ready.
+#[derive(RustcEncodable, RustcDecodable)]
+pub struct Ready {
+    pub id: ID,
 }
