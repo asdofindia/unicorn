8f3dc1b49a422c40ed3a316c4a6aec687e0376b2
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-04-12 15:41:30 +0530

    Improve enums & structs in messages::common
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- src/messages/common.rs
+++ src/messages/common.rs
@@ -2,33 +2,47 @@
 #![allow(unstable)]
 
 
-/// List of main components in unicorn
-#[derive(RustcEncodable, RustcDecodable)]
+/// List of main component types in unicorn
+#[derive(RustcEncodable, RustcDecodable, Debug)]
 pub enum Components {
-    ClientAdapter,
-    Core,
-    DataElement,
-    DataHandler,
-    DataStore,
-    Gateway,
+    Core = 0,
+    Gateway = 1,
+    DataStore = 2,
+    DataElement = 3,
+    DataHandler = 4,
+    ClientAdapter = 5,
+}
+
+/// Different states in which a component can be at any given time
+#[derive(RustcEncodable, RustcDecodable, Debug)]
+pub enum State {
+    CONNECTING = 0,
+    READY = 1,
+    BUSY = 2,
+    RETRYING = 3,
+    SUCCESS = 4,
+    FAILED = 5,
+    OFFLINE = 99,
 }
 
 /// Identify message. Used by every message that needs an ID
-#[derive(RustcEncodable, RustcDecodable)]
+#[derive(RustcEncodable, RustcDecodable, Debug)]
 pub struct ID {
-    pub id: String,
+    pub uuid: String,
     pub component: Components,
 }
 
 /// Heartbeat message. Used to check liveliness of connected peers.
-#[derive(RustcEncodable, RustcDecodable)]
+#[derive(RustcEncodable, RustcDecodable, Debug)]
 pub struct Heartbeat {
     pub id: ID,
     pub count: i32,
 }
 
-/// Ready message. Sent when a component has connected and is ready.
-#[derive(RustcEncodable, RustcDecodable)]
-pub struct Ready {
+/// Status message. Used to send status of different components
+#[derive(RustcEncodable, RustcDecodable, Debug)]
+pub struct Status {
     pub id: ID,
+    pub state: State,
+    pub msg: Option<String>,
 }
