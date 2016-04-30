04ad61aaf33985b4da45c4df62b35b077013c8c4
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-04-30 17:38:42 +0530

    Update core::server to work with modified network::Net
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- src/core/server.rs
+++ src/core/server.rs
@@ -11,10 +11,9 @@ pub fn run() {
     // Max port: 65535 (u16 MAX)
     let addr = "127.0.0.1:60000".to_string();
 
-    let mut net = Net::new(&addr);
-    match net.bind() {
-        Ok(_) => {
-            println!("[core] Listening on {}", &addr);
+    match Net::bind(addr) {
+        Ok(net) => {
+            println!("[core] Listening on {}", &net.addr);
             net.recv(&processor::process_msg);
         }
         Err(_) => println!("[core] Error binding listener"),
