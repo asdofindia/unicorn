c6976622fbbef0f009d79df9c05eaaeab46b7ef9
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-04-30 13:05:01 +0530

    Implement stream reconnection without recursion
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- src/network/stream.rs
+++ src/network/stream.rs
@@ -15,16 +15,35 @@ impl Stream {
     /// Create a new stream
     pub fn new(stream: TcpStream) -> Stream { Stream { stream: stream } }
 
-    /// Connect to a TCP listener socket and get back a stream
-    pub fn connect(addr: &String, retry: bool) -> Result<Stream, Error> {
+    /// Try connecting to remote TCP socket only once
+    fn connect_once(addr: &String) -> Result<Stream, Error> {
         match TcpStream::connect(&addr[..]) {
             Ok(s) => Ok(Stream::new(s)),
             Err(e) => {
                 println!("[net] Unable to connect to link");
+                Err(e)
+            }
+        }
+    }
+
+    /// Keep trying for connection in a loop
+    fn connect_loop(addr: &String) -> Result<Stream, Error> {
+        loop {
+            println!("Retrying...");
+            if let Ok(s) = Stream::connect_once(addr) {
+                return Ok(s)
+            }
+            sleep(Duration::from_secs(1));
+        }
+    }
+
+    /// Connect to a TCP listener socket and get back a stream
+    pub fn connect(addr: &String, retry: bool) -> Result<Stream, Error> {
+        match Stream::connect_once(addr) {
+            Ok(s) => Ok(s),
+            Err(e) => {
                 if retry {
-                    println!("Retrying...");
-                    sleep(Duration::from_secs(1));
-                    return Stream::connect(addr, true)
+                    return Stream::connect_loop(addr)
                 }
                 Err(e)
             }
